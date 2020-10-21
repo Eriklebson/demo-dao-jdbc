0786ccdaa33e4f95b7652458c885741540c6248b
Eriklebson <eriklebson2016@gmail.com>
2020-10-21 18:46:54 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/modal/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,40 @@
+package modal.dao.impl;
+
+import java.util.List;
+
+import modal.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao{
+
+	@Override
+	public void insert(Seller obs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller obs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findByIn(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
